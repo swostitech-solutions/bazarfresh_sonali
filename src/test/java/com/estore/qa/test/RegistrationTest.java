7d94eaf3735e70b218b0981e07f4a09d7ffc77af
SonaliDas <sonalidas1177@gmail.com>
2024-04-05 18:11:07 +0530

    Commit

--- src/test/java/com/estore/qa/test/RegistrationTest.java
+++ src/test/java/com/estore/qa/test/RegistrationTest.java
@@ -5,37 +5,36 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
 import com.estore.qa.basetest.BaseTest;
 import com.estore.qa.commonutility.Xls_Reader;
 import com.estore.qa.pages.EStoreRegistrationPage;
 
-public class RegistrationTest extends BaseTest{
+public class RegistrationTest extends BaseTest {
 	EStoreRegistrationPage registrationPage;
-	@BeforeMethod
-	public void setUP()
-	{
-		initialization();
-		registrationPage=new EStoreRegistrationPage();
-	}
-	
-	@Test
-	public void VerifyRegistrationTest() throws Exception
-	{
 
-		Xls_Reader reader=new Xls_Reader("D:\\Automation\\GitAutomation\\bazarfresh_sonali\\src\\main\\java\\com\\estore\\qa\\testdata\\EStoreAppdata.xlsx");
-		String moduleName="Registration";
-		
-		String firstName=reader.getCellData(moduleName, "FirstName", 2);
+	@BeforeMethod
+	public void setUP() {
+		initialization();
+		registrationPage = new EStoreRegistrationPage();
+	}
+
+	@Test
+	public void VerifyRegistrationTest() throws Exception {
+
+		Xls_Reader reader = new Xls_Reader(
+				"D:\\Automation\\GitAutomation\\bazarfresh_sonali\\src\\main\\java\\com\\estore\\qa\\testdata\\EStoreAppdata.xlsx");
+		String moduleName = "Registration";
+
+		String firstName = reader.getCellData(moduleName, "FirstName", 2);
 		System.out.println(firstName);
-		String lastName=reader.getCellData(moduleName, "LastName", 2);
+		String lastName = reader.getCellData(moduleName, "LastName", 2);
 		System.out.println(lastName);
-		String email=reader.getCellData(moduleName, "EmailId", 2);
+		String email = reader.getCellData(moduleName, "EmailId", 2);
 		System.out.println(email);
-		String telephone=reader.getCellData(moduleName, "Telephone", 2);
-		String password=reader.getCellData(moduleName, "Password", 2);
-		String confirmpassword=reader.getCellData(moduleName, "ConfirmPassword", 2);
-		
+		String telephone = reader.getCellData(moduleName, "Telephone", 2);
+		String password = reader.getCellData(moduleName, "Password", 2);
+		String confirmpassword = reader.getCellData(moduleName, "ConfirmPassword", 2);
+
 		registrationPage.clickOnSignUp();
 		registrationPage.EnterFirstName(firstName);
 		registrationPage.EnterLastName(lastName);
@@ -47,17 +46,11 @@ public class RegistrationTest extends BaseTest{
 		registrationPage.clickOnContinue();
 		Thread.sleep(10000);
 
-		
+	}
 
-}
 	@AfterMethod
-	public void tearDown()
-	{
+	public void tearDown() {
 		driver.quit();
 	}
-	public void tearup() {
-		driver.quit();
-	}
-	
-	
+
 }
